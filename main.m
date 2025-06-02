717aeb0e0ade48e02029cd4474a924dc1c60e8f6
kuternega <aleksandr.kuternega02@gmail.com>
2025-06-02 20:49:57 +0300

    Добавлена возможность сохранения части полученных результатов в случае возникновения ошибок.

--- main.m
+++ main.m
@@ -4,12 +4,12 @@ params.name = {'H', 'L', 'gamma', 'V0', 'C0', 'T0', 'iav'};
 params.step = [0.003, 0.01, pi/12, 0.00005, 50, 15, 10];
 params.tol = [0.0005, 0.001, pi/32, 0.00001, 1, 1, 1];
 
-file_name = 'data output/data_output_H.csv';
+file_name = 'data output.csv';
 maxIter = 5;
 timeout = 10*60;
 func = @(param) f(param);
 data = load_data(file_name);
-[x_min, f_min, data] = coordDescentGoldenAuto(func, params, maxIter, timeout, data);  
+[x_min, f_min, data] = coordDescentGoldenAuto(func, params, maxIter, timeout, data, file_name);  
 fprintf('\n\n\nОптимальное значение параметров:\n');
 for i = 1:length(x_min.value)
     fprintf('%s = %f\n', x_min.name{i}, x_min.value(i));
@@ -40,12 +40,22 @@ function [data] = load_data(file_name)
 end
 
 
-function [x_best, f_best, data] = coordDescentGoldenAuto(f, x, maxIter, timeout, data)
+function [x_best, f_best, data] = coordDescentGoldenAuto(f, x, maxIter, timeout, data, file_name)
     n = numel(x.value);
+    ok = true;
     for i = 1:n
         fprintf('\n\n\nОптимизация параметра: %s\n', x.name{i});
-        % Определение границ
-        [a, b, tmp] = findBracket(@(val) f(setCoord(x, i, val)), x.value(i), x.step(i), maxIter, timeout, data(i));
+        % Определение границ        
+        try
+            if i > length(data)
+                data(i).x = [];
+                data(i).y = [];
+            end
+            [a, b, tmp] = findBracket(@(val) f(setCoord(x, i, val)), x.value(i), x.step(i), maxIter, timeout, data(i));
+        catch ME
+            error = ME;
+            ok = false;            
+        end
         data(i).x = tmp.x;
         data(i).y = tmp.y;
         fprintf('\nИнтервал поиска:\t\t[%f; %f]\n', a, b);
@@ -53,29 +63,42 @@ function [x_best, f_best, data] = coordDescentGoldenAuto(f, x, maxIter, timeout,
         fprintf('Минимальная длина интервала:\t\t%f\n', x.tol(i));
         % Оптимизация по i-й координате
         fi = @(xi) f(setCoord(x, i, xi));
-        [x.value(i), tmp] = goldenSection(fi, a, b, x.tol(i), data(i));
-        data(i).x = tmp.x;
-        data(i).y = -tmp.y;
+        if ok
+            try
+                [x.value(i), tmp] = goldenSection(fi, a, b, x.tol(i), data(i));
+            catch ME
+                error = ME;
+                ok = false;                 
+            end        
+            data(i).x = tmp.x;
+            data(i).y = -tmp.y;
+        end
         figure;
         plot(data(i).x, data(i).y, 'o');
         xlabel(x.name(i));
         ylabel('Изменение потока соли');
         grid on;
-        allData = [];
-        % сохранение данных в csv файл
-        for j = 1:length(data)
-            lenX = length(data(j).x);  % длина векторов
-            temp = table(...
-                repmat(j, lenX, 1), ...
-                data(j).x(:), ...
-                data(j).y(:), ...
-                'VariableNames', {'param', 'x', 'y'});
-            allData = [allData; temp];
+        if ~ok
+            break;
         end
-        writetable(allData, 'data_output.csv');
     end
 
-    
+    % сохранение данных в csv файл
+    allData = [];        
+    for j = 1:length(data)
+        lenX = length(data(j).x);  % длина векторов
+        temp = table(...
+            repmat(j, lenX, 1), ...
+            data(j).x(:), ...
+            data(j).y(:), ...
+            'VariableNames', {'param', 'x', 'y'});
+        allData = [allData; temp];
+    end
+    if ~ok
+        rethrow(error);
+    end
+    writetable(allData, file_name);
+        
     x_best = x;
     [f_best, ~] = evalWithTimeout(@() f(x_best), -1);
     f_best = -f_best;
@@ -87,116 +110,135 @@ function x_new = setCoord(x, i, val)
 end
 
 function [xmin, data] = goldenSection(f, a, b, tol, data)
-    gr = (sqrt(5) + 1) / 2;
-    c = b - (b - a) / gr;
-    d = a + (b - a) / gr;
-    while abs(b - a) > tol        
-        [fc, ~] = evalWithTimeout(@() f(c), -1);
-        [fd, ~] = evalWithTimeout(@() f(d), -1);
-        data.x(end + 1) = c;
-        data.y(end + 1) = fc;
-        data.x(end + 1) = d;
-        data.y(end + 1) = fd;
-        if fc < fd
-            b = d;
-        else
-            a = c;
-        end
-        fprintf('\nИнтервал поиска:\t\t[%f; %f]\n', a, b);
-        fprintf('Длина интервала:\t\t%f\n', b - a);
-        fprintf('Минимальная длина интервала:\t\t%f\n', tol);
+    try
+        gr = (sqrt(5) + 1) / 2;
         c = b - (b - a) / gr;
         d = a + (b - a) / gr;
-    end
+        while abs(b - a) > tol        
+            [fc, ~] = evalWithTimeout(@() f(c), -1);
+            [fd, ~] = evalWithTimeout(@() f(d), -1);
+            data.x(end + 1) = c;
+            data.y(end + 1) = fc;
+            data.x(end + 1) = d;
+            data.y(end + 1) = fd;
+            if fc < fd
+                b = d;
+            else
+                a = c;
+            end
+            fprintf('\nИнтервал поиска:\t\t[%f; %f]\n', a, b);
+            fprintf('Длина интервала:\t\t%f\n', b - a);
+            fprintf('Минимальная длина интервала:\t\t%f\n', tol);
+            c = b - (b - a) / gr;
+            d = a + (b - a) / gr;
+        end
 
-    xmin = (a + b) / 2;
+        xmin = (a + b) / 2;
+    catch ME
+        xmin = a;
+        data = data;
+        rethrow(ME);
+    end
 end
 
 function [a_out, b_out, data] = findBracket(f1d, x0, step, maxIter, timeout, data)
-    % f1d — функция от 1 переменной
-    % timeout — максимальное время для вызова функции
-    % min_step — минимально допустимый шаг
-    [fx0, ok] = evalWithTimeout(@() f1d(x0), timeout);
-    if ~ok
-        error('Не удалось вычислить начальное значение функции');
-    end
-    data.x(end + 1) = x0;
-    data.y(end + 1) = fx0;
-    % поиск правой границы
-    [b_out, data] = findBracketR(f1d, x0, step, maxIter, timeout, data, fx0);
-    fprintf('\nНайдена правая граница:\t\t%f\n', b_out);
-    % поиск левой границы
-    [a_out, data] = findBracketR(f1d, x0, -step, maxIter, timeout, data, fx0);
-    fprintf('\nНайдена левая граница:\t\t%f\n', a_out);
+    try
+        % f1d — функция от 1 переменной
+        % timeout — максимальное время для вызова функции
+        % min_step — минимально допустимый шаг
+        [fx0, ok] = evalWithTimeout(@() f1d(x0), timeout);
+        if ~ok
+            error('Не удалось вычислить начальное значение функции');
+        end
+        data.x(end + 1) = x0;
+        data.y(end + 1) = fx0;
+        % поиск правой границы
+        [b_out, data] = findBracketR(f1d, x0, step, maxIter, timeout, data, fx0);
+        fprintf('\nНайдена правая граница:\t\t%f\n', b_out);
+        % поиск левой границы
+        [a_out, data] = findBracketR(f1d, x0, -step, maxIter, timeout, data, fx0);
+        fprintf('\nНайдена левая граница:\t\t%f\n', a_out);
+    catch ME
+        data = data;
+        a_out = x0;
+        b_out = x0;
+        rethrow(ME);
+    end 
 end
 
 function [b_out, data] = findBracketR(f1d, x0, step, maxIter, timeout, data, fa)
-    min_step = step/2;
-    stepR = step;
-    b = x0 + stepR;
-    [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
-    if ok
-        data.x(end + 1) = b;
-        data.y(end + 1) = fb;
-    end
-    if ~ok
+    try
+        min_step = step/2;
         stepR = step;
-        while ~ok && stepR > min_step
-            stepR = max(abs(stepR * 0.5), abs(min_step));  % Шаг не может быть меньше min_step
-            b = x0 + stepR;
-            [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
-            if ok
-                data.x(end + 1) = b;
-                data.y(end + 1) = fb;
-            end
-        end
-        if ~ok
-            b_out = x0;
-            return
-        end
-    end
-    if fb >= fa
-        b_out = b;
-        return
-    end
-    iter = 0;
-    while ok && fa >= fb
-        stepR = stepR * 2;  
-        b = x0 - stepR;
+        b = x0 + stepR;
         [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
         if ok
             data.x(end + 1) = b;
             data.y(end + 1) = fb;
         end
-        iter = iter + 1;
-        if iter > maxIter
-            if ok
-                b_out = x0 + stepR;
+        if ~ok
+            stepR = step;
+            while ~ok && stepR > min_step
+                stepR = max(abs(stepR * 0.5), abs(min_step));  % Шаг не может быть меньше min_step
+                b = x0 + stepR;
+                [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
+                if ok
+                    data.x(end + 1) = b;
+                    data.y(end + 1) = fb;
+                end
+            end
+            if ~ok
+                b_out = x0;
                 return
             end
-            b_out = x0 + stepR/2;
+        end
+        if fb >= fa
+            b_out = b;
             return
         end
-    end
-    if ~ok
-        stepR2 = step;
-        while ~ok && stepR > min_step
-            stepR2 = max(abs(stepR * 0.5), abs(min_step));  % Шаг не может быть меньше min_step
-            b = x0 + stepR + stepR2;
+        iter = 0;
+        while ok && fa >= fb
+            stepR = stepR * 2;  
+            b = x0 - stepR;
             [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
             if ok
                 data.x(end + 1) = b;
                 data.y(end + 1) = fb;
             end
+            iter = iter + 1;
+            if iter > maxIter
+                if ok
+                    b_out = x0 + stepR;
+                    return
+                end
+                b_out = x0 + stepR/2;
+                return
+            end
         end
         if ~ok
-            b_out = x0 + stepR;
+            stepR2 = step;
+            while ~ok && stepR > min_step
+                stepR2 = max(abs(stepR * 0.5), abs(min_step));  % Шаг не может быть меньше min_step
+                b = x0 + stepR + stepR2;
+                [fb, ok] = evalWithTimeout(@() f1d(b), timeout);
+                if ok
+                    data.x(end + 1) = b;
+                    data.y(end + 1) = fb;
+                end
+            end
+            if ~ok
+                b_out = x0 + stepR;
+                return
+            end
+            b_out = x0 + stepR + stepR2;
             return
         end
-        b_out = x0 + stepR + stepR2;
-        return
+        b_out = x0 + stepR;    
+    catch ME
+        b_out = x0;
+        data = data;
+        rethrow(ME);
     end
-    b_out = x0 + stepR;    
 end
 
 
